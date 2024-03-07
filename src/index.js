e44b5495ca1c84a5c72fcf5ed0c9fbd2a9d44913
Evgeniia Zhavoronkova <maongaku@gmail.com>
2024-03-07 19:32:12 +0100

    feat: add styles for process, testimonials sections

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 import "./scss/style.scss";
+
+const questions = document.querySelectorAll(".process-item__title");
+
+questions.forEach(function (question) {
+  const btn = question.querySelector(".question-btn");
+  console.log(btn);
+
+  btn.addEventListener("click", function () {
+    //console.log(question);
+
+    questions.forEach(function (item) {
+      if (item !== question) {
+        item.classList.remove("show-text");
+      }
+    });
+
+    question.classList.toggle("show-text");
+  });
+});
