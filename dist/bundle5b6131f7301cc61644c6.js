21109a84eb2d32f1c6e25a37dad78f9afb01cd2b
Evgeniia Zhavoronkova <maongaku@gmail.com>
2024-03-12 12:49:39 +0100

    chores: add dist folder

---
+++ dist/bundle5b6131f7301cc61644c6.js
@@ -0,0 +1,2 @@
+(()=>{"use strict";window.addEventListener("DOMContentLoaded",(function(){var t;(t=document.querySelectorAll(".process-item")).forEach((function(e){e.querySelector(".question-btn").addEventListener("click",(function(){t.forEach((function(t){t!==e&&t.classList.remove("show-text")})),e.classList.toggle("show-text")}))}))}))})();
+//# sourceMappingURL=bundle5b6131f7301cc61644c6.js.map
